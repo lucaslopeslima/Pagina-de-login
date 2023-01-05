a9950994ab593c95f40526546e74f2e92b141d51
Lucas L. Lima <lucaslima.webdev@gmail.com>
2023-01-05 03:35:56 -0300

    feat- userName local storage

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -2,7 +2,23 @@ console.log('JS on')
 
 ///////Handle Buttons ///////
 const sphereToggle = document.querySelector('.remember-user-name')
-console.log(sphereToggle)
+//console.log(sphereToggle)
 sphereToggle.addEventListener('click', () => {
     sphereToggle.classList.toggle('remember-user-name-active')
+})
+
+const logarBtn = document.querySelector('.logar-btn')
+const inputEmail = document.querySelector('#email')
+inputEmail.value = localStorage.getItem('userName')
+//console.log(inputEmail)
+//console.log(logarBtn)
+logarBtn.addEventListener('click', () => {
+    console.log(sphereToggle.classList)
+    if (sphereToggle.classList.contains('remember-user-name-active')) {
+        console.log('tem remember-user-name-active')
+        localStorage.setItem('userName', inputEmail.value);
+    }
+    else {
+        console.log('não tem remember-user-name-active')
+    }
 })
