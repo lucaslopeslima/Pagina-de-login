8ad1c6f96562c3ea86c03c6938088df8cc6a558f
Lucas L. Lima <lucaslima.webdev@gmail.com>
2023-01-21 21:31:13 -0300

    feat- npm firebase

--- assets/js/firebase.mjs
+++ assets/js/firebase.mjs
@@ -9,6 +9,7 @@ firebase.initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
+const auth = firebase.auth();
 console.log(db)
 
 
