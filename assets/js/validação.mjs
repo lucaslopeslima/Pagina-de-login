318d9263dd216e6a4f0bb896057157292b5fa1fb
Lucas L. Lima <lucaslima.webdev@gmail.com>
2023-01-23 22:04:54 -0300

    feat- validate psw

--- assets/js/validação.mjs
+++ assets/js/validação.mjs
@@ -6,8 +6,22 @@ function validateEmail(email) {
 }
 
 function validatePassword(password) {
-    if (password.map.lenght < 6) {
+    if (password.length < 6) {
         return false
     }
     return true
+}
+
+function validateNewUser(user, password1, password2) {
+    if (validateEmail(user) == false) {
+        return false
+    } if (validatePassword(password1) == true) {
+        if (validatePassword(password2) == true) {
+            if (password1 !== password2) {
+                return false
+            }
+            return true
+        }
+    } 
+
 }
