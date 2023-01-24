c84445f7679570dc0e32d488d9decd39d894fa8a
Lucas L. Lima <lucaslima.webdev@gmail.com>
2023-01-23 21:53:10 -0300

    feat- new structure

--- assets/js/validação.mjs
+++ assets/js/validação.mjs
@@ -1,1 +1,13 @@
 console.log('validação ON')
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    if (password.map.lenght < 6) {
+        return false
+    }
+    return true
+}
